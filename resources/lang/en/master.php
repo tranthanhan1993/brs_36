ddc4949284e66f7dd3fce0670cc34191295e9bf3
Phu Hoa <phuhoadn95@gmail.com>
2016-12-07 13:17:53 +0700

    create file lang - css

---
+++ resources/lang/en/master.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'category' => 'Category',
+    'activities_of_follower' => 'Activities Of Follower',
+    'login' => 'Log in',
+    'logout' => 'Log out',
+    'profile' => 'Profile',
+    'update' => 'Update',
+    'favorite_book' => 'Favorite Book',
+    'more' => 'More',
+    'list_follow' => 'List Follow',
+    'follow' => 'Follow',
+    'unfollow' => 'Unfollow',
+    'request' => 'Request',
+    'search' => 'Search',
+    'public_library' =>'Public Library',
+    'all_rights_reserved' =>'All Rights Reserved',
+    'public_policy' =>'Privacy Policy ',
+    'privacy_policy' =>'Privacy Policy',
+    'rights_reserved' =>'Rights Reserved ',
+    'history' => 'History',
+    'follower' => 'Follower',
+    'time_line' => 'Time line',
+];
