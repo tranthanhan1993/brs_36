933b75decdd5e3c8916ebc13e8899c3034db69ce
Tony Tran <tranthanhan1993@gmail.com>
2016-12-11 20:32:25 +0700

    add AdminMiddleware and RepositoryUser

---
+++ resources/lang/en/message.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'create_error' => 'Error in create process',
+    'login_error' => 'Login Error,Please try again',
+    'edit_error' => 'Error in edit process',
+    'delete_error' => 'Error in delete process',
+    'update_error' => 'Error in update process',
+    'show_error' => 'Error in show process',
+    'find_error' => 'Find Id Error,Please try again',
+    'edit_user_successfully' => 'Edit user profile successfully',
+];
