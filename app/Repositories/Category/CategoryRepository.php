58dac19eb863fd54683d69366722ccc2eb1792dc
Tony Tran <tranthanhan1993@gmail.com>
2016-12-20 10:58:16 +0700

    make profile user

--- app/Repositories/Category/CategoryRepository.php
+++ app/Repositories/Category/CategoryRepository.php
@@ -3,14 +3,21 @@ namespace App\Repositories\Category;
 
 use App\Models\Category;
 use App\Repositories\BaseRepository;
+use App\Repositories\Book\BookRepository;
+use App\Repositories\Review\ReviewRepository;
 
 class CategoryRepository extends BaseRepository
 {
     protected $model;
 
-    public function __construct(Category $category)
-    {
+    public function __construct(
+        Category $category,
+        BookRepository $bookRepository,
+        ReviewRepository $reviewRepository
+    ) {
         $this->model = $category;
+        $this->bookRepository = $bookRepository;
+        $this->reviewRepository = $reviewRepository;
     }
 
     public function getListCategory()
@@ -24,5 +31,23 @@ class CategoryRepository extends BaseRepository
         $categories = $this->model->all();
 
         return $categories;
+    }
+
+    public function delete($id)
+    {
+        $category = $this->model->find($id);
+        if ($category) {
+            foreach ($category->books as $book) {
+                foreach ($book->reviews as $review) {
+                    $this->reviewRepository->delete($review->id);
+                }
+                $this->bookRepository->delete($book->id);
+            }
+            $category->delete();
+
+            return true;
+        }
+
+        return false;
     }
 }
